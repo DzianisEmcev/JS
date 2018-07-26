ac1126831bd4f52d5dc28ae65894ecd65a034e68
DzianisEmcev <edenis@tut.by>
2018-07-27 02:32:42 +0300

    Test router react post + comments

--- src/components/posts/TestRouter.js
+++ src/components/posts/TestRouter.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 export class TestRouter extends Component {
+
     render() {
+        const Idcomments = this.props.match.params.postId;
+        fetch(`https://jsonplaceholder.typicode.com/posts/${Idcomments}/comments`).then(response => {
+            return response.json();
+        }).then(comments => {
+            this.setState({comments});
+        });
         return(
-            <p>Test router: {this.props.match.params.postId}</p>
+            <p>{this.props.match.params.posts}</p>
         );
     }
 }
